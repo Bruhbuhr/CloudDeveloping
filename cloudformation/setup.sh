297bc8bf839dc0fba17518fe69cb85c095ffeb84
KhangLeonardo <s3929994@rmit.edu.vn>
2025-01-09 07:27:53 +0000

    add automation in EC2 instance feature

--- cloudformation/setup.sh
+++ cloudformation/setup.sh
@@ -1,21 +1,21 @@
-AWS_PROFILE=default
-PREFIX=asm3
-ENV=
-KEYPAIR=
+AWS_PROFILE=
+PREFIX=team2-asm3
+ENV=dev
+KEYPAIR=team2-asm3-key
 IAM_ROLE=LabRole
 IAM_INSTANCE_PROFILE=LabInstanceProfile
 
 # Compute resources
 aws cloudformation create-stack \
-    --stack-name $PREFIX-$ENV-compute-stack \
+    --stack-name $PREFIX-compute-$ENV-stack \
     --template-body file://compute-resources.yaml \
-    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=KeyPair,ParameterValue=$KEYPAIR ParameterKey=InstanceProfile,ParameterValue=$IAM_INSTANCE_PROFILE \
+    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=KeyPair,ParameterValue=$KEYPAIR ParameterKey=InstanceProfile,ParameterValue=$IAM_INSTANCE_PROFILE ParameterKey=Role,ParameterValue=$IAM_ROLE \
     --profile=$AWS_PROFILE
 
 aws cloudformation update-stack \
-    --stack-name $PREFIX-$ENV-compute-stack \
+    --stack-name $PREFIX-compute-$ENV-stack \
     --template-body file://compute-resources.yaml \
-    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=KeyPair,ParameterValue=$KEYPAIR ParameterKey=InstanceProfile,ParameterValue=$IAM_INSTANCE_PROFILE \
+    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=KeyPair,ParameterValue=$KEYPAIR ParameterKey=InstanceProfile,ParameterValue=$IAM_INSTANCE_PROFILE ParameterKey=Role,ParameterValue=$IAM_ROLE \
     --profile=$AWS_PROFILE
 
 # Serverless resources
@@ -31,11 +31,28 @@ aws cloudformation update-stack \
     --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=Role,ParameterValue=$IAM_ROLE \
     --profile=$AWS_PROFILE
 
+# Pipeline resources
+aws cloudformation create-stack \
+    --stack-name $PREFIX-pipeline-$ENV-stack \
+    --template-body file://pipeline-resources.yaml \
+    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=Role,ParameterValue=$IAM_ROLE \
+    --profile=$AWS_PROFILE
+
+aws cloudformation update-stack \
+    --stack-name $PREFIX-pipeline-$ENV-stack \
+    --template-body file://pipeline-resources.yaml \
+    --parameters ParameterKey=Prefix,ParameterValue=$PREFIX ParameterKey=Env,ParameterValue=$ENV ParameterKey=Role,ParameterValue=$IAM_ROLE \
+    --profile=$AWS_PROFILE
+
 # Clean resources
 aws cloudformation delete-stack \
-    --stack-name $PREFIX-$ENV-compute-stack \
+    --stack-name $PREFIX-compute-$ENV-stack \
     --profile=$AWS_PROFILE
 
 aws cloudformation delete-stack \
     --stack-name $PREFIX-serverless-$ENV-stack \
+    --profile=$AWS_PROFILE
+
+aws cloudformation delete-stack \
+    --stack-name $PREFIX-pipeline-$ENV-stack \
     --profile=$AWS_PROFILE
